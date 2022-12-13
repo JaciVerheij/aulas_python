2c696a331533d34a3220eb1d8bca05b984fb4be4
Jaci Verheij <120043285+JaciVerheij@users.noreply.github.com>
2022-12-12 21:44:08 -0300

    Atividade Python realizadas no dia 12/12/2022

---
+++ atividadepratica1.py
@@ -0,0 +1,10 @@
+cpf = "951.753.826-74"
+nome = "Jaci"
+sobrenome = "Verheij"
+valorfiat = 21587.24
+valormoto = 14366.31
+
+print (cpf+"/"+nome+" "+sobrenome)
+print (f"A quantidade de digitos é de: {len(cpf)}")
+print (f"A ultima letra do nome é: {nome[3]}")
+print (f"Eu {nome} {sobrenome} do CPF: {cpf} estou declarando que recebi no mês de Novembro/22 a quantia de R${valorfiat} na venda de uma Fiat Uno 99 e R${valormoto} na venda da MotoX1, somando a quantia de R${valorfiat+valormoto}")
